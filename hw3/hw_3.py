0dee7ad1e47858d628b07c31db36fa6a49ad6f62
macoo0906 <127402507+macoo0906@users.noreply.github.com>
2023-03-24 01:35:50 +0900

    Add files via upload

---
+++ hw3/hw_3.py
@@ -0,0 +1,9 @@
+import get_fixed_price as fixed
+
+per = float(input("할인율은? "))
+fixed.get_fixed_price(per)
+
+A_price = float(input('A 상품의 할인된 가격은?'))
+B_price = float(input("B 상품의 할인된 가격은?"))
+print('A 상품의 정가는',fixed.A_price(A_price),'원')
+print('B 상품의 정가는',fixed.B_price(B_price),'원')
