08f34f13c039627b70db14968a6c1eaaf431ae80
macoo0906 <127402507+macoo0906@users.noreply.github.com>
2023-03-24 01:30:31 +0900

    Update get_fixed_price.py

--- hw3/get_fixed_price.py
+++ hw3/get_fixed_price.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 per = 0
 
 def get_fixed_price(percent) : 
@@ -11,17 +10,3 @@ def A_price(A) :
 
 def B_price(B) :
     return B / (100 - per) * 100
-=======
-per = 0
-
-def get_fixed_price(percent) : 
-    global per
-    per = percent
-
-
-def A_price(A) : 
-    return A / (100 - per) * 100
-
-def B_price(B) :
-    return B / (100 - per) * 100
->>>>>>> 6014907dff120bf1e8ad591944ec489d7dcdb54b
