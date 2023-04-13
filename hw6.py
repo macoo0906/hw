a60005315971cf758b788766435bfc3f2f3a267f
macoo0906 <127402507+macoo0906@users.noreply.github.com>
2023-04-13 20:08:24 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,12 @@
+for j in range(1, 10, 1):
+    for i in range(2, 6, 1):
+        print(f' {i} x {j} =  { i * j :2d}', "\t", end = "")
+    print(" ")
+
+print()
+
+for j in range(1,10) :
+    for i in range(6,10) : 
+        print(f' {i} X {j} = { i * j :2d}', "\t", end='')
+    print(" ")
+
